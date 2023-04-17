62395264a601575ce98db1804cb427ea0e65ef01
SOOS-JAlvarez <92373106+SOOS-JAlvarez@users.noreply.github.com>
2023-04-17 09:56:46 -0300

    PA-9115 disable rules specifically for each mode (#65)

--- hooks/soos_dast_hook.py
+++ hooks/soos_dast_hook.py
@@ -6,6 +6,7 @@ import helpers.constants as Constants
 import sys
 import traceback
 from helpers.utils import log, exit_app
+from typing import List
 
 config = DASTConfig()
 
@@ -29,9 +30,13 @@ def zap_started(zap, target):
             target = target[0:target.index('/', 8) + 1]
 
         zap.ascan.update_scan_policy(scanpolicyname=Constants.ZAP_ACTIVE_SCAN_POLICY_NAME, attackstrength="LOW")
-        log(f"disabled rules: {config.disable_rules}")
-        zap.pscan.disable_scanners(','.join(config.disable_rules))
-        zap.ascan.disable_scanners(','.join(config.disable_rules), Constants.ZAP_ACTIVE_SCAN_POLICY_NAME)
+
+        if config.disable_rules:
+            pscan_disabled_rules = set(config.disable_rules).intersection(set(_all_passive_scanner_rules(zap)))
+            ascan_disabled_rules = set(config.disable_rules).intersection(set(_all_active_scanner_rules(zap, Constants.ZAP_ACTIVE_SCAN_POLICY_NAME)))
+            zap.pscan.disable_scanners(','.join(pscan_disabled_rules))
+            zap.ascan.disable_scanners(','.join(ascan_disabled_rules), Constants.ZAP_ACTIVE_SCAN_POLICY_NAME)
+            log(f"disabled rules: {config.disable_rules}")
 
         auth = DASTAuth(config)
         auth.authenticate(zap, target)
@@ -43,10 +48,13 @@ def zap_started(zap, target):
 
     return zap, target
 
-
 def zap_pre_shutdown(zap):
     log("Overview of spidered URL's:")
     with open('spidered_urls.txt', 'w') as f:
         for url in zap.spider.all_urls:
             f.write(f"{url}\n")
             log(f"found: {url}")
+
+def _all_active_scanner_rules(zap, policy_name) -> List[str]: return [scanner['id'] for scanner in zap.ascan.scanners(policy_name)]
+
+def _all_passive_scanner_rules(zap) -> List[str]: return [scanner['id'] for scanner in zap.pscan.scanners]
