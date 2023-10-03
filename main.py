29fead512720d5b395bd71dc9857588305607e8c
SOOS-GSteen <gsteen@soos.io>
2023-10-03 15:16:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1057,10 +1057,9 @@ class SOOSDASTAnalysis:
                 raise Exception(f"An Unexpected error has occurred running the {self.scan_mode} scan")
 
             # Add the discovered urls to the report
+            discoveredUrls = []
             if (os.path.isfile('./spidered_urls.txt')):
                 discoveredUrls = open('./spidered_urls.txt', 'r').read().splitlines()
-            else:
-                discoveredUrls = []
             data = json.load(open(Constants.REPORT_SCAN_RESULT_FILE, 'r'))
             data['discoveredUrls'] = discoveredUrls
             json.dump(data, open(Constants.REPORT_SCAN_RESULT_FILE, 'w'))
