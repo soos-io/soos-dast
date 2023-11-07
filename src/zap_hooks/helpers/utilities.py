ff8ca3b347ce2466573096ec87e88a6029ada8f7
SOOS-JAlvarez <jalvarez@soos.io>
2023-11-07 10:38:15 -0300

    removed stuff without reference from the hook

--- src/zap_hooks/helpers/utilities.py
+++ src/zap_hooks/helpers/utilities.py
@@ -1,15 +1,9 @@
 from sys import exit
 from typing import Dict, Iterable, NoReturn
 
-from requests import Response
-from requests.exceptions import HTTPError
-
 import src.zap_hooks.helpers.constants as Constants
 from src.zap_hooks.model.log_level import LogLevel, loggerFunc
-from src.zap_hooks.model.target_availability_check import TargetAvailabilityCheck
 
-
-UTF_8: str = 'utf-8'
 
 def log(message: str, log_level: LogLevel = LogLevel.INFO) -> None:
     logFunc = loggerFunc.get(log_level)
@@ -21,30 +15,9 @@ def print_line_separator() -> None:
         "----------------------------------------------------------------------------------------------------------"
     )
 
-
 def exit_app(e) -> NoReturn:
     log(str(e), LogLevel.ERROR)
     exit(1)
-
-
-def _check_status(response: Response) -> TargetAvailabilityCheck:
-    try:
-        response.raise_for_status()
-    except HTTPError as error:
-        log(f"{type(error).__name__}: {response.status_code}")
-        log(response.text, log_level=LogLevel.DEBUG)
-
-        # 401 status indicates the host is available but may be behind basic auth
-        if response.status_code == 401:
-            return TargetAvailabilityCheck(True, response=response)
-
-        return TargetAvailabilityCheck(
-            False,
-            response=response,
-            unavailable_reason=error,
-        )
-    else:
-        return TargetAvailabilityCheck(True, response=response)
 
 
 def process_custom_cookie_header_data(data: str) -> Dict:
