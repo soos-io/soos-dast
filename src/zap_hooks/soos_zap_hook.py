efa4554094fbe801eced460903e37d1ca1c3dc02
SOOS-JAlvarez <jalvarez@soos.io>
2023-12-21 16:52:42 -0300

    saving work, bunch of debug lines

--- src/zap_hooks/soos_zap_hook.py
+++ src/zap_hooks/soos_zap_hook.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import traceback
 from typing import List
@@ -59,6 +60,30 @@ def zap_import_context(zap, context_file):
     zap.context.remove_context(globals.context_name)
 
 def zap_pre_shutdown(zap):
+    log("printing context list")
+    log(zap.context.context_list)
+    log("saving session")
+    log(zap.core.save_session(name="session_file_test.session", overwrite=True))
+    zap.context.export_context(globals.context_name, "wrk/context_file_test.context")
+
+    log("printing rules configuration")
+    log(zap.ruleConfig.all_rule_configs)
+
+    # Retrieve specific configuration settings
+    spider_max_duration = zap.spider.option_max_duration
+    scanner_threads_per_host = zap.ascan.option_thread_per_host
+    ascan_max_duration = zap.ascan.option_max_scan_duration_in_mins
+    ascan_max_result = zap.ascan.option_max_results_to_list
+    ascan_max_rule_duration = zap.ascan.option_max_rule_duration_in_mins
+    log(f"Current directory is {os.getcwd()}")
+    log(f"printinf files in current directory: {os.listdir()}")
+    # Write configuration settings to a file
+    with open('wrk/zap_config_settings.txt', 'w') as config_file:
+        config_file.write(f"Spider Max Duration: {spider_max_duration}\n")
+        config_file.write(f"Scanner Threads Per Host: {scanner_threads_per_host}\n")
+        config_file.write(f"Active Scan Max Duration: {ascan_max_duration}\n")
+        config_file.write(f"Active Scan Max Result: {ascan_max_result}\n")
+        config_file.write(f"Active Scan Max Rule Duration: {ascan_max_rule_duration}\n")
     log("Overview of spidered URL's:")
     with open('spidered_urls.txt', 'w') as f:
         for url in zap.spider.all_urls:
