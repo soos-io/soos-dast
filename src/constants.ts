6de964299272f6307e20b6b0af5d32d20070b330
SOOS-GSteen <gsteen@soos.io>
2024-12-18 16:51:05 -0500

    package update - new cmd line options (#152)
    package update - new cmd line options

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const SOOS_DAST_CONSTANTS = {
   AuthDelayTime: 5,
   Tool: "zap",
-  ToolVersion: "2.15", // NOTE: this should match the zap-stable version
+  ToolVersion: "2.15", // NOTE: this should match the zap-stable version in the Docker file
   Files: {
     DiscoveredUrlsFile: "./core_urls.txt",
     ReportScanResultFile: "/zap/wrk/report.zap.json",
