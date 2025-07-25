f784bd58a50e9f69adec9f35272cafb0685fdfbb
SOOS-GSteen <gsteen@soos.io>
2025-07-25 12:45:06 -0400

    package update / eslint (#215)
    package update / eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,31 @@ import tseslint from "typescript-eslint";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{mjs,ts}"] },
-  { languageOptions: { globals: globals.node } },
+  {
+    ignores: ["build/**"],
+  },
+  {
+    languageOptions: {
+      globals: globals.node,
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["eslint.config.mjs", "jest.config.ts"],
+          defaultProject: "tsconfig.json",
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unnecessary-condition": "error",
+      "@typescript-eslint/default-param-last": "error",
+      "@/no-param-reassign": "error",
+      "@/prefer-const": "error",
+    },
+  },
 ];
