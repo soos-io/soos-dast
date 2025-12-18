d434d9b7f9fd10a9823f6b0f7090b45cd6b5b1c7
SOOS-GSteen <gsteen@soos.io>
2025-12-18 14:27:09 -0500

    zaproxy/zap-stable:2.17.0

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const SOOS_DAST_CONSTANTS = {
   AuthDelayTime: 5,
   Tool: "zap",
-  ToolVersion: "2.16.1", // NOTE: this should match the zap-stable version in the Docker file
+  ToolVersion: "2.17.0", // NOTE: this should match the zap-stable version in the Docker file
   Files: {
     DiscoveredUrlsFile: "./core_urls.txt",
     ReportScanResultFile: "/zap/wrk/report.zap.json",
